7c1369ec49975bfef2258dfea6cac321f749ba7a
Nasif Fuad <nasifuad91@gmail.com>
2023-12-26 21:55:23 +0600

    first commit

---
+++ contact-us.php
@@ -0,0 +1,29 @@
+<?php include './components/header.php'; ?>
+
+<div class="ui container">
+
+    <!-- Top Navigation Bar -->
+    <?php include './components/top-menu.php'; ?>
+
+    <!-- BODY Content -->
+    <div class="ui grid">
+        <!-- Left menu -->
+        <?php include './components/side-menu.php'; ?>
+
+        <!-- right content -->
+        <div class="twelve wide column">
+            <h1>Contact Us</h1>
+
+
+            <p><strong>Address:</strong><br> Demo- Mr ABC <br>Dhaka , Bangldesh </p>
+
+            <p><strong>Email:</strong> abc@gmail.com</p>
+            <p><strong>Tel:</strong> +018273764673</p>
+
+            <span class="p-20"></span>
+        </div>
+    </div>
+
+</div>
+
+<?php include './components/footer.php'; ?>
